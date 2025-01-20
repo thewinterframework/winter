68790b75b29afd5d8a77b5b9784ad141d078523f
diegonighty <diego.cardenas.v06@gmail.com>
2025-01-20 17:04:57 -0600

    feat: sub modules now are in another repositories!

--- sponge-configurate/src/main/java/com/thewinterframework/configurate/feedback/media/MediaType.java
+++
@@ -1,9 +0,0 @@
-package com.thewinterframework.configurate.feedback.media;
-
-public enum MediaType {
-    CHAT,
-    TITLE,
-    ACTIONBAR,
-    BOSSBAR,
-    SOUND
-}
--- test-paper/src/main/java/com/thewinterframework/test/TestPlugin.java
+++
@@ -1,8 +0,0 @@
-package com.thewinterframework.test;
-
-import com.thewinterframework.paper.PaperWinterPlugin;
-import com.thewinterframework.plugin.WinterBootPlugin;
-
-@WinterBootPlugin
-public class TestPlugin extends PaperWinterPlugin {
-}
